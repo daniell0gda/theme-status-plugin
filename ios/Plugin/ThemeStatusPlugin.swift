2ffdcab1d01e9db0156511a94f4c94b31df5eb84
daniell0gda <ferens.daniel@gmail.com>
2023-04-29 18:13:19 +0200

    fix: not calling from main thread

--- ios/Plugin/ThemeStatusPlugin.swift
+++ ios/Plugin/ThemeStatusPlugin.swift
@@ -6,26 +6,28 @@ import Capacitor
 public class ThemeStatusPlugin: CAPPlugin {
 
     @objc func isDarkMode(_ call: CAPPluginCall) {
-        let currentWindow = UIApplication.shared.windows.first { $0.isKeyWindow }
-        if let style = currentWindow?.traitCollection.userInterfaceStyle {
-            switch style {
-            case .light:
-                call.resolve([
-                    "value": false
-                ])
-            case .dark:
-                call.resolve([
-                    "value": true
-                ])
-            case .unspecified:
+        DispatchQueue.main.async {
+            let currentWindow = UIApplication.shared.windows.first { $0.isKeyWindow }
+            if let style = currentWindow?.traitCollection.userInterfaceStyle {
+                switch style {
+                case .light:
+                    call.resolve([
+                        "value": false
+                    ])
+                case .dark:
+                    call.resolve([
+                        "value": true
+                    ])
+                case .unspecified:
+                    call.resolve([
+                        "value": "undefined"
+                    ])
+                }
+            } else {
                 call.resolve([
                     "value": "undefined"
                 ])
             }
-        } else {
-            call.resolve([
-                "value": "undefined"
-            ])
         }
     }
 }
